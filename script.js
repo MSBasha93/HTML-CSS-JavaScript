9ae054b37e93be8f8edef6c836359fa4fbca8e24
Mahmoud Soltan <mahmoud.t.soltan@gmail.com>
2022-06-16 02:48:14 +0200

    Suggested fixes batch #1

--- script.js
+++ script.js
@@ -1,62 +1,55 @@
-const imgArr=["./num0.jpg", "./num1.png", "./num2.png", "./num3.png", "./num4.png", "./num5.png", "./num6.jpeg"
-, "./num7.png", "./num8.png", "./num9.jpg", "./num10.jpg", "./num11.png", "./num12.png", "./num13.png"
-, "./num14.png", "./num15.png", "./num16.jpeg", "./num17.png", "./num18.png", "./num19.jpg"];
-var clickedNumArr=[]; 
-var clickedIdArr=[];
-var randImgArr=[ "./num0.jpg","./num1.png", "./num2.png", "./num3.png", "./num4.png", "./num5.png", "./num6.jpeg"
-, "./num7.png", "./num8.png", "./num9.jpg","./num10.jpg", "./num11.png", "./num12.png", "./num13.png"
-, "./num14.png", "./num15.png", "./num16.jpeg", "./num17.png", "./num18.png", "./num19.jpg"];
+const imgArr = Array(20).fill().map((_, i) => `./num${i}.png`);
+
+let clickedNumArr=[];
+let clickedIdArr=[];
+
+let randImgArr = imgArr.slice();
 
 // Randomize img locations 
 function shuffle(o) {
-  for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-  return o;
+  return o.sort(() => (Math.random() > 0.5) ? 1 : -1);
 };
-var random = shuffle(randImgArr);
-var timeOut;
 
+let random = shuffle(randImgArr);
+let timeOut;
 
-//Game play code
+// Game play code
 const images = document.querySelectorAll("#images img");
-    //flip when clicked
-    images.forEach(img => 
-      img.onclick= function () {
-        clearTimeout(timeOut);
-        this.src=randImgArr[this.id];
-        clickedNumArr.unshift(imgArr.indexOf(randImgArr[this.id]));
-        clickedIdArr.unshift(randImgArr[this.id]);
-        console.log(clickedNumArr)
-        console.log(clickedIdArr)
-        //check matching if two cards are clicked
-        if(clickedNumArr[1]!=null){
-          //check to delete if matching
-          if(clickedNumArr[0]==clickedNumArr[1]-10 || clickedNumArr[1]==clickedNumArr[0]-10 ){
-            
-            document.getElementById(randImgArr.indexOf(clickedIdArr[0])).classList.add("delete");
-            document.getElementById(randImgArr.indexOf(clickedIdArr[1])).classList.add("delete");
-            clickedNumArr.length=0;
-            clickedIdArr.length=0;
-              
-            
-          }
-          //check to reflip if unmatching  
-          else if(clickedNumArr[0]!=clickedNumArr[1]-10 || clickedNumArr[1]!=clickedNumArr[0]-10){
-            
-           setTimeout(()=>{
-           document.getElementById(randImgArr.indexOf(clickedIdArr[0])).src="./back.png";
-           document.getElementById(randImgArr.indexOf(clickedIdArr[1])).src="./back.png";
-           
-           clickedNumArr.length=0;
-           clickedIdArr.length=0;
-          },500);
-          
-          }
-     
-        }
-      
-      
-      }
-    ); 
 
-     
-//decision making is to be based on string value of img.src directly 
+// Flip when clicked
+images.forEach(img => 
+  img.addEventListener('click', function (event) {
+    clearTimeout(timeOut);
+    this.src = randImgArr[this.id];
+    clickedNumArr.unshift(imgArr.indexOf(randImgArr[this.id]));
+    clickedIdArr.unshift(randImgArr[this.id]);
+    console.log(clickedNumArr)
+    console.log(clickedIdArr)
+    // Check matching if two cards are clicked
+    if (clickedNumArr[1] != null) {
+      if (clickedNumArr[0] == clickedNumArr[1]-10 || clickedNumArr[1] == clickedNumArr[0]-10) {
+        // Delete if matching
+        document.getElementById(randImgArr.indexOf(clickedIdArr[0])).classList.add("delete");
+        document.getElementById(randImgArr.indexOf(clickedIdArr[1])).classList.add("delete");
+        clickedNumArr.length = 0;
+        clickedIdArr.length = 0;
+      } else if (clickedNumArr[0] != clickedNumArr[1]-10 || clickedNumArr[1] != clickedNumArr[0]-10) {
+        // Reflip if unmatching
+
+        // TODO: We should disable the event listener (onclick) here so that people can't click during the timeout.
+
+        setTimeout(()=>{
+          document.getElementById(randImgArr.indexOf(clickedIdArr[0])).src="./back.png";
+          document.getElementById(randImgArr.indexOf(clickedIdArr[1])).src="./back.png";
+
+          // TODO: Then re-enable it here, so that people can click cards again.
+
+          clickedNumArr.length=0;
+          clickedIdArr.length=0;
+        },500);
+      }
+    }
+  })
+);
+
+// Decision making is to be based on string value of img.src directly
